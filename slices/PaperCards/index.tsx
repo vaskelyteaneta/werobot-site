304fb01c3675bbee21c3aa493ceb0239b396e830
vaskelyteaneta <vaskelyteaneta@gmail.com>
2026-02-23 12:51:01 +0100

    Remove white background from PaperCards image container

--- slices/PaperCards/index.tsx
+++ slices/PaperCards/index.tsx
@@ -82,7 +82,7 @@ const PaperCards: FC<PaperCardsProps> = ({ slice }) => {
         }}
       >
         {/* Always use the same document icon */}
-        <div className="w-full aspect-square overflow-hidden flex items-center justify-center bg-white border border-black">
+        <div className="w-full aspect-square overflow-hidden flex items-center justify-center bg-transparent border border-black">
           <img
             src="/file image1.png"
             alt="Paper document"
